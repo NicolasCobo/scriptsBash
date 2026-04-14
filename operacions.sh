42be9f72282bf4bac8ed907265a3b7ce12dce587
Nicolas Cobo <ncobo@esliceu.net>
2026-04-14 16:37:39 +0200

    Video 07

---
+++ operacions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+#variables són "paraules" que contenen un valor
+NOM="Nicolas"
+#read serveix per llegir informació del teclat
+read -p "Introdueix un nom: " NOM
+
+
+echo "El teu nom és ${NOM}"
