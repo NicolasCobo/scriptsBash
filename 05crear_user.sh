7b942a19f2e08f403c945f945023af26164fb365
Nicolas Cobo <ncobo@esliceu.net>
2026-04-15 15:44:11 +0200

    video 14

--- 05crear_user.sh
+++ 05crear_user.sh
@@ -4,7 +4,7 @@ function usage(){
     echo "Usage ${0}"
     echo
     echo "has de ser root"
-    exit 1
+    exit 1 #indica que hi ha agut una errada
 }
 
 
@@ -18,7 +18,32 @@ then
     usage
 fi
 
+read -p "Introdueix el nom complet: " COMMENTS
+#demanam el nom de l'usuari
 read -p "Introduex el nom d'usuari: " USER_NAME
+#demanam el password
+read -p "Introduex el password: " PASSWORD
 
-echo "creant l'usuari ${USER_NAME}"
+#creariem l'usuari
+useradd -m -c "${COMMENTS}" ${USER_NAME} &> /dev/null
+#control de la darrera comanda
+if [[ ${?} -ne 0 ]]  
+then 
+    echo "Errada creant l'usuari"
+    exit 1
+fi
+
+#canvi de password
+echo "${USER_NAME}:${PASSWORD}"  | chpasswd
+#comprovam si el canvi de password ha anat be
+if [[ ${?} -ne 0 ]]
+then
+    echo "Errada canviant passwd"
+    exit 1
+fi
+
+#fer canviar el passwd
+passwd -e ${USER_NAME}
+
+
 
