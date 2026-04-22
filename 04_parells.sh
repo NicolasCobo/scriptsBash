8f0c28575db83ea9de6856a7084133884007a073
Nicolas Cobo <ncobo@esliceu.net>
2026-04-22 15:45:58 +0200

    activitat4 parells i senars

--- 04_parells.sh
+++ 04_parells.sh
@@ -2,7 +2,31 @@
 
 MODUL=$(($1 % 2))
 
-echo "modul val $MODUL"
+
+#CONTROL D'ERROR:
+if [[ ${#} -eq 0 ]]
+then
+    echo "ERROR:Has d'introduir 1 numero com a minim"
+    exit 1
+fi
+
+PARELLS=0
+SENARS=0
+
+
+while [ ${#} -gt 0 ]; do 
+    if [ $((${1} % 2)) -eq 0 ];
+    then
+        PARELLS=$((PARELLS+1))
+    else
+        SENARS=$((SENARS+1))
+    fi
+    shift
+done
+
+echo "Hi ha ${PARELLS} nÚmeros parells"
+echo "Hi ha ${SENARS} nÚmeros senars"
+
 
 
 
